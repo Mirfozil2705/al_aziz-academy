490a22e8d328bf45d9637c0a4b767bfeb4bb0341
Mirfozil Xakimov <110598264+Mirfozil2705@users.noreply.github.com>
2022-08-04 14:09:31 +0500

    Add files via upload

---
+++ Dasturlash 05 Al-aziz academy/js/script.js
@@ -0,0 +1,11 @@
+function BurgerActiv(){
+    let Burger = document.querySelector('.burger_logo');
+    let menyu = document.querySelector('.menu');
+    let menu_4_blok = document.querySelector('.menu_4_blok');
+    Burger.classList.toggle('activ');
+    menyu.classList.toggle('activ');
+    menu_4_blok.classList.toggle('activ');
+}
+
+    
+
